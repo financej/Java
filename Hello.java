9863c70f9b568e2c775e8d52262ae39eb65718ec
Minjun Jang <77034309+financej@users.noreply.github.com>
2021-09-06 22:02:59 +0900

    ex01\

---
+++ Hello.java
@@ -0,0 +1,21 @@
+
+public class Hello {
+
+	public static void main(String[] args) /* 프로그램의 시작 */ 
+	{
+		// TODO Auto-generated method stub
+		// ctrl + shift + L 단축키 전체 목록보기
+		// ctrl + +, - 폰트 크기 증가 / 감소
+		// ctrl + D 한줄 삭제
+		// ctrl + alt + down 행단위 복사
+		// alt + shift + A 멀티 칼럼 편집
+		// alt + up, down 행단위 이동
+		// ctrl + i 자동 들여쓰기
+		// ctrl + / 주석 (토글) -> 한줄 주석
+		// ctrl + space 자동완성
+		System.out.println("Hello, world"); // Hello, world를 출력
+		System.out.println("Hello, /* 이것은 주석이 아님 */ world");
+		System.out.println("Hello, world // 이것도 주석 아님");		
+	}
+
+}
